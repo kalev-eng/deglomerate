085f95b8ad4d2eb99a65d5f9b23aa2f3e72e600d
kalev-eng <kalev.eng@gmail.com>
2022-05-16 19:02:01 -0400

    airdrop update

--- evm/scripts/airdrop.js
+++ evm/scripts/airdrop.js
@@ -8,6 +8,11 @@ const hre = require("hardhat");
 async function main() {
   const BackgroundLogoElement = await hre.ethers.getContractFactory("BackgroundLogoElement");
   const backgroundLogoElement = await BackgroundLogoElement.attach("0xfedf7d1f0bde821407B4906482e0d94CE2B04b1c");
+
+  const Logos = await hre.ethers.getContractFactory("Logos");
+  const logos = await Logos.attach("0x320CCb623b612Eef62F186AfF6b664377cb4260b");
+
+  /*
   const contractAddress = '0xd65c5D035A35F41f31570887E3ddF8c3289EB920';
   const erc = await hre.ethers.getContractFactory("ERC721A");
   const ethTContract = await erc.attach(contractAddress);
@@ -25,14 +30,19 @@ async function main() {
   for (let i = 0; i < distributionList.length; i++) {
     console.log(distributionList[i]);
   }
+  */
   
+  var distributionList = ['0xa9d2F90BD250E82E5ddc0d14d27063de21768848', '0x5578851e8c3e7520485dc8cDd6B13049D078bc8e']
   var thisBatch = [];
   for (let i = 0; i < distributionList.length; i++) {
     thisBatch.push(distributionList[i]);
-    if (thisBatch.length % 25 == 0) {
+    if (thisBatch.length % 2 == 0) {
       console.log('Airdropping for bucket...')
-      console.log(thisBatch)
+      console.log(thisBatch);
+      await backgroundLogoElement.mintAdmin(thisBatch, 1, { gasLimit : 200000 });
+      await logos.mintAdmin(thisBatch, 1, { gasLimit : 200000 });
       // await backgroundLogoElement.mintAdmin(thisBatch, 1, { gasLimit : 1875000 });
+      // await logos.mintAdmin(thisBatch, 1, { gasLimit : 1875000 });
       thisBatch = [];
     }
   }
